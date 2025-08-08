140a6f76ff5b0454cd5afd14abf62af3653c66b0
Jonathan Parra <58872902+jondeparra@users.noreply.github.com>
2025-08-08 08:49:12 +0200

    Finished Day 21

--- Project02-GuessTheFlag/Project02-GuessTheFlag/ContentView.swift
+++ Project02-GuessTheFlag/Project02-GuessTheFlag/ContentView.swift
@@ -8,62 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
 
     var body: some View {
-        VStack {
-            VStack(alignment: .center) {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("This is a VStack")
-            }
-            .padding()
-
-            HStack(alignment: .center) {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("This is an HStack")
-            }
-            .padding()
-
-            ZStack {
-                Color(red: 0.5, green: 0.8, blue: 0.6)
-                    .frame(width: 200, height: 200)
-                Text("This is a ZStack")
-            }
-            .padding()
-
-            ZStack {
-                LinearGradient(colors: [.red, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .frame(width: 200, height: 200)
-
-                Text("This is a gradient")
-                    .foregroundStyle(.white)
-            }
-            .padding()
+        ZStack {
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3), .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
 
             VStack {
-                Button("Add element") {
-                    showingAlert = true
+                Spacer()
+
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+
+                Spacer()
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.white)
+
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                        
+                    }
                 }
-                    .buttonStyle(.borderedProminent)
+                .frame(maxWidth: .infinity)
+                .padding(30)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
 
-                Button("Delete selection", role: .destructive) { }
-                    .buttonStyle(.bordered)
+                Spacer()
 
-                Button("Edit element") { }
-                    .buttonStyle(.bordered)
+                Text("Score: ???")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+
+                Spacer()
+                Spacer()
             }
-            .padding()
+            .padding(.horizontal, 30)
         }
-        .alert("This is an alert", isPresented: $showingAlert) {
-            Button("Okay") { }
-            Button("Cancel", role: .cancel) { }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
         } message: {
-            Text("You can add a title and body message to an alert.")
+            Text("Your score is ???")
         }
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
